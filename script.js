7717d7e43c69ff06e87d216582edd1d468d5339a
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-21 16:21:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here!
+let infiList=document.getElementById('infi-list')
+
+let n=10
+for(let i=1;i<=n;i++){
+	let list=document.createElement('li')
+	list.textContent=`Item ${i}`
+	infiList.appendChild(list)
+}
+
+infiList.addEventListener('scroll', function() {
+    var a = infiList.scrollTop;
+    var b = infiList.scrollHeight - infiList.clientHeight;
+    // var c = a / b;
+
+     if (a + infiList.clientHeight >= infiList.scrollHeight * 0.9) {
+        // Add 2 more items when the user scrolls near the bottom
+        for (let i = n + 1; i <= n + 2; i++) {
+            let list = document.createElement('li');
+            list.textContent = `Item ${i}`;
+            infiList.appendChild(list);
+        }
+        
+        // Update n after adding the new items
+        n += 2;
+    }
+});
+
 
